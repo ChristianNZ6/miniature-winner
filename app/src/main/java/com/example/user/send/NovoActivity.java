ff6862287610c6544f8c2221f8bcc31b12589883
Christian <you@example.comchristianjorge2009@gmail.com>
2018-05-11 01:28:46 -0300

    planejamento inicial modificado e acrescentamento de 4 novas telas

---
+++ app/src/main/java/com/example/user/send/NovoActivity.java
@@ -0,0 +1,25 @@
+package com.example.user.send;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class NovoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo);
+    }
+
+    public void salva (View view){
+
+
+
+    }
+    public void cancelar (View view){
+
+    finish();
+
+    }
+}
---
+++ app/src/main/java/com/example/user/send/Usuario.java
@@ -0,0 +1,21 @@
+package com.example.user.send;
+
+public class Usuario extends _Default {
+
+
+    private int id;
+    private String nome;
+    private String numero;
+    private String mensagem;
+    public Usuario(){
+        super();
+        this.id = -1;
+        this.nome="";
+        this.numero="";
+        this.mensagem="";
+
+
+    }
+
+
+}
