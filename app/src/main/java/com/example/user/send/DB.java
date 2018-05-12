26a4f9b4674f4284d34347f50d3c91a49555806e
ChristianNZ6 <christianjorge2009@gmail.com>
2018-05-12 17:35:47 -0300

    conexão implementada

--- app/src/main/java/com/example/user/send/DB.java
+++ app/src/main/java/com/example/user/send/DB.java
@@ -1,18 +1,100 @@
 package com.example.user.send;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 
-public class DB extends _Default {
+public class DB extends _Default implements Runnable {
 
     private Connection conn;
-    private String host = "";
+    private String host = "localhost";
     private String db = "android";
-    private int port = 2345;
+    private int port = 3306;
     private String user = "android";
-    private String pass = "*android*";
+    private String pass = "";
+    private String url = "jdbc:postgresql://%s:%d/%s";
 
     public DB() {
 
         super();
+        this.url = String.format(this.url,this.host,this.port,this.db);
+
+        this.conecta();
+       this.disconecta();
+
+    }
+
+    @Override
+    public void run() {
+       try{
+           Class.forName("org.postgresql.Driver");
+           this.conn = DriverManager.getConnection(this.url,this.user,this.pass);
+
+
+
+
+       }catch (Exception e){
+
+           this._mensagem = e.getMessage();
+           this._status = false;
+
+
+       }
+    }
+
+
+    private void conecta(){
+
+Thread thread = new Thread(this);
+thread.start();
+
+try {
+    thread.join();
+
+}catch (Exception e){
+    this._mensagem = e.getMessage();
+    this._status = false;
+
+
+}
+    }
+
+    private void disconecta() {
+        if(this.conn != null ){
+
+            try {
+
+                this.conn.close();
+
+            }catch (Exception e){
+
+
+            }finally {
+                this.conn = null;
+            }
+
+        }
+    }
+    public ResultSet select (String query) {
+        this.conecta();
+        ResultSet resultSet = null;
+        try {
+            resultSet = new ExecuteDB(this.conn, query).execute().get();
+        }catch (Exception e){
+            this._status = false;
+            this._mensagem = e.getMessage();
+        }
+        return resultSet;
+    }
+    public ResultSet execute (String query) {
+        this.conecta();
+        ResultSet resultSet = null;
+        try {
+            resultSet = new ExecuteDB(this.conn, query).execute().get();
+        }catch (Exception e){
+            this._status = false;
+            this._mensagem = e.getMessage();
+        }
+        return resultSet;
     }
 }
